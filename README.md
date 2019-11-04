10a2ef6376bbc1dc05402f6991bc220dd619fdb9
Kurt Brendlinger <kurt.brendlinger@gmail.com>
2019-11-04 10:17:34 +0000

    first import

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# this corresponds to /home/kurtbrendlinger
+cd $HOME 
+mkvirtualenv dashappenv --python=/usr/bin/python3.7
+pip install -r mysite/requirements3.7.txt
---
+++ kurtbrendlinger_pythonanywhere_com_wsgi.py
@@ -0,0 +1,22 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://<your-username>.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Flask project
+
+import sys
+
+# add your project directory to the sys.path
+project_home = u'/home/kurtbrendlinger/mysite'
+if project_home not in sys.path:
+    sys.path = [project_home] + sys.path
+
+# import flask app but need to call it "application" for WSGI to work
+#from flask_app import app as application  # noqa
+
+# The above is modified to accommodate a Dash application
+from flask_app import app
+application = app.server
+
+# Hopefully this file does not need to be updated very often.
