2b87434d716c9596c1f092a45c4a52eb408b9980
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2019-11-16 18:46:49 +0100

    figure out the global variables, and how they relate to the callbacks

--- flask_app.py
+++ flask_app.py
@@ -16,6 +16,7 @@ import base64
 import datetime
 import io
 
+import numpy as np
 import pandas as pd
 import dash_table
 
@@ -24,43 +25,39 @@ from dash.dependencies import Input, Output, State
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# start out with some data
-global_df = pd.read_json('Tidepool_Export_191104_191110.json')
-
-global_smbg = global_df[global_df['type'] == 'smbg'][['deviceTime', 'value']][:5]
-
-#print(global_df[global_df['type'] == 'smbg'][['deviceTime', 'value']][:5],file=sys.stdout)
+# Make some globals that we can overwrite, refer to, etc.
+my_globals = dict()
+my_globals['global_df'] = 0
+my_globals['pd_smbg'] = 0
 
 # for deployment, pass app.server (which is the actual flask app) to WSGI etc
 app = dash.Dash(external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     children=[
-#     html.Link(rel="icon", href="images/shortcut2.png"),
 
         html.H1(children='Modeling blood glucose and the effects of insulin'),
 
-        html.Div(children='''
-Upload your data below:
-'''
-                 ),
+        html.Div(children='''The following works with Tidepool and Medtronic 551.'''),
 
         dcc.Upload(id='upload-data',
-                   children=html.Div(['Drag and Drop or ',html.A('Select Files')]),
-                   style={'width': '100%',
-                          'height': '60px',
-                          'lineHeight': '60px',
-                          'borderWidth': '1px',
-                          'borderStyle': 'dashed',
-                          'borderRadius': '5px',
-                          'textAlign': 'center',
-                          'margin': '10px'
-                          },
-                   # Allow multiple files to be uploaded
-                   multiple=False
+                   children=[html.Button('Upload your own data from Tidepool')],
+                   multiple=False,
                    ),
 
-        html.Div(id='output-data-upload',children = [
+        html.Div(id='uploaded-input-data-flag',children=None),
+
+        html.Div(children=['''Pick a date:''',
+                           dcc.DatePickerSingle(id='my-date-picker-single',
+                                                min_date_allowed=datetime.datetime(1995, 8, 5),
+                                                max_date_allowed=datetime.datetime(2017, 9, 19),
+                                                initial_visible_month=datetime.datetime(2017, 8, 5),
+                                                date=str(datetime.datetime(2017, 8, 25, 23, 59, 59))
+                                                ),
+                           ]
+                 ),
+
+        html.Div(children = [
                 html.Hr(),  # horizontal line
                 dcc.Graph(id='display-tidepool-graph',
                           #config={'staticPlot':True,},
@@ -95,58 +92,46 @@ def update_output_div(input_value):
 #
 # How we parse the input file contents
 #
-def parse_contents(contents, filename, date):
-    # return html.Div(children = 'Hello there!')
+def process_input_file(contents, filename, date):
+
     content_type, content_string = contents.split(',')
-
-    #print(content_string[:300],file=sys.stdout)
-
     decoded = base64.b64decode(content_string)
+
     try:
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
-            global_df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+            my_globals['global_df'] = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
-            global_df = pd.read_excel(io.BytesIO(decoded))
+            my_globals['global_df'] = pd.read_excel(io.BytesIO(decoded))
         elif 'json' in filename :
-            global_df = pd.read_json(io.BytesIO(decoded))
+            my_globals['global_df'] = pd.read_json(io.BytesIO(decoded))
 
     except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
+        print(e,file=sys.stdout)
+        return 'There was an error processing this file.'
 
-    df_smaller = global_df[['deviceTime','value','subType']]
-    data_dict = df_smaller.to_dict('records')
-    #print(data_dict,file=sys.stdout)
-
-    # return the figure feature of the graph
-    return {'data': [{'x': [1, 2, 3], 'y': [2, 2, 2], 'type': 'scatter', 'name': 'Montreal updated!'},
-                     ],
-            'layout': {'title': 'UPDATED'}
-            }
+    return 'Using new file, named {}'.format(filename)
 
 #
-# Upload callback
+# Upload callback (new)
 #
-@app.callback(Output('display-tidepool-graph', 'figure'),
+@app.callback(Output('uploaded-input-data-flag', 'children'),
               [Input('upload-data', 'contents')],
               [State('upload-data', 'filename'),
                State('upload-data', 'last_modified')])
-def update_output(list_of_contents, list_of_names, list_of_dates):
+def update_file(list_of_contents, list_of_names, list_of_dates):
+
+    text_outputs = []
 
     if list_of_contents is None:
-        return {'data': [#{'x': global_smbg['deviceTime'], 'y': global_smbg['value'], 'type': 'scatter', 'name': 'SF','mode':'markers'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'scatter', 'name': 'defaultreal'},
-                {'x': [1, 2, 3], 'y': [5, 3, 2], 'type': 'scatter', 'name': 'Defaultreal 2'},
-                ],
-                'layout': {'title': 'Default for None'
-                           }
-                }
+        # Use the default file
+        my_globals['global_df'] = pd.read_json('Tidepool_Export_191104_191110.json')
+        list_of_names = ''
+        list_of_dates = ''
+        text_outputs = ['Using default file.']
 
-    if list_of_contents is not None:
+    else :
 
         if type(list_of_contents) != type([]) :
             list_of_contents = [list_of_contents]
@@ -161,10 +146,39 @@ def update_output(list_of_contents, list_of_names, list_of_dates):
         print('list of filenames:',list_of_names, file=sys.stdout)
         print('list of last_modified:',list_of_dates, file=sys.stdout)
 
-        children = [
-            parse_contents(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
-            ]
-        return children[0]
+        text_outputs = list(process_input_file(c, n, d) for c, n, d in zip(list_of_contents, list_of_names, list_of_dates))
+
+        find_errors = list('error' in a for a in text_outputs)
+        if (True in find_errors) :
+            return text_outputs[find_errors.index(True)]
+
+    print('updating global_smbg',file=sys.stdout)
+    my_globals['pd_smbg'] = my_globals['global_df'][my_globals['global_df']['type'] == 'smbg'][['deviceTime', 'value']]
+
+    return '. '.join(text_outputs)
+
+#
+# Update the plot
+#
+@app.callback(Output('display-tidepool-graph', 'figure'),
+              [Input('uploaded-input-data-flag', 'children')])
+def update_plot(update_indicator):
+
+    if update_indicator == None :
+        # Don't worry - this will be updated by default from the Upload callback
+        return {}
+
+    print('children:',update_indicator,file=sys.stdout)
+    print(my_globals['pd_smbg'][:10],file=sys.stdout)
+    print('first device time:',my_globals['pd_smbg']['deviceTime'].iloc[0],file=sys.stdout)
+
+    return {'data': [
+            {'x': my_globals['pd_smbg']['deviceTime'], 'y': np.round(my_globals['pd_smbg']['value']*18.01559), 'type': 'scatter', 'name': 'SF','mode':'markers'},
+            ],
+            'layout': {'title': 'Default for None'
+                       }
+            }
+
 
 app.title = 'Kurt Webpage'
 
