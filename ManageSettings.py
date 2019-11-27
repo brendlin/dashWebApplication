b7851c2f5834267a389047c6b73521c43fec08d0
Kurt Brendlinger <kurt.brendlinger@cern.ch>
2019-11-27 10:33:46 +0100

    Add settings table, populate bwz settings

---
+++ ManageSettings.py
@@ -0,0 +1,75 @@
+from BGModel import Settings
+import datetime
+
+def LoadFromJsonData(data,globals) :
+
+    settings_frame = None
+
+    if 'basal' in data.columns :
+        settings_frame = data[(data['type'] == 'pumpSettings') & (data['basal'].notnull())]
+    else :
+        print('Warning: no pump settings are available apparently.',file=sys.stdout)
+        return
+        
+    globals['pd_settings'] = settings_frame
+    globals['bwz_settings'] = []
+
+    settings_sensitivity = Settings.UserSetting('Sensitivity')
+    settings_ric = Settings.UserSetting('RIC')
+    settings_duration = Settings.UserSetting('Duration')
+    settings_basal = Settings.UserSetting('Basal')
+
+    last_bas = dict()
+    last_sens = dict()
+    last_carb = dict()
+    last_dur = 0
+
+    for i in reversed(range(len(settings_frame))) :
+
+        print('checking',settings_frame.iloc[i]['deviceTime'])
+
+        tmp_bas  = settings_frame.iloc[i]['basalSchedules']['standard']
+        tmp_sens = settings_frame.iloc[i]['insulinSensitivity']
+        tmp_carb = settings_frame.iloc[i]['carbRatio']
+        tmp_dur  = settings_frame.iloc[i]['bolus']['calculator']['insulin']['duration']
+
+        if (tmp_bas == last_bas and tmp_sens == last_sens and 
+            tmp_carb == last_carb and tmp_dur == last_dur) :
+            continue
+        else :
+            last_bas  = tmp_bas
+            last_sens = tmp_sens
+            last_carb = tmp_carb
+            last_dur  = tmp_dur
+
+        print('New settings from',settings_frame.iloc[i]['deviceTime'])
+        deviceTime = settings_frame.iloc[i]['deviceTime']
+
+        for j in settings_frame.iloc[i]['basalSchedules']['standard'] :
+            #print('start:',datetime.timedelta(milliseconds=j['start']),'rate:',round(j['rate'],2))
+            timeOfDay_hr = datetime.timedelta(milliseconds=j['start']).total_seconds()/3600.
+            settings_basal.AddSettingToSnapshot(deviceTime,timeOfDay_hr,round(j['rate'],2))
+
+        for j in settings_frame.iloc[i]['insulinSensitivity'] :
+            #print('start:',datetime.timedelta(milliseconds=j['start']),'amount:',round(j['amount']*18.01559,2))
+            timeOfDay_hr = datetime.timedelta(milliseconds=j['start']).total_seconds()/3600.
+            settings_sensitivity.AddSettingToSnapshot(deviceTime,timeOfDay_hr,round(j['amount']*18.01559,2))
+
+        for j in settings_frame.iloc[i]['carbRatio'] :
+            #print('start:',datetime.timedelta(milliseconds=j['start']),'amount:',round(j['amount'],2))
+            timeOfDay_hr = datetime.timedelta(milliseconds=j['start']).total_seconds()/3600.
+            settings_ric.AddSettingToSnapshot(deviceTime,timeOfDay_hr,round(j['amount'],2))
+
+        #print(settings_frame.iloc[i]['bolus']['calculator']['insulin']['duration'])
+        settings_duration.AddSettingToSnapshot(deviceTime,0,settings_frame.iloc[i]['bolus']['calculator']['insulin']['duration'])
+
+        the_userprofile = Settings.TrueUserProfile()
+        the_userprofile.AddSensitivityFromArrays(settings_sensitivity.latestSettingsSnapshot(),
+                                                 settings_ric.latestSettingsSnapshot())
+        the_userprofile.AddHourlyGlucoseFromArrays(settings_basal.latestSettingsSnapshot(),
+                                                   settings_duration.latestSettingsSnapshot())
+        the_userprofile.Print()
+        globals['bwz_settings'].append(the_userprofile)
+
+    print('settings done.')
+
--- flask_app.py
+++ flask_app.py
@@ -19,6 +19,13 @@ import io
 import numpy as np
 import pandas as pd
 import dash_table
+import copy
+
+# Tools
+import ManageSettings
+
+# BG Classes
+from BGModel import BGActionClasses
 
 # needed for callbacks
 from dash.dependencies import Input, Output, State
@@ -29,6 +36,24 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 my_globals = dict()
 my_globals['global_df'] = 0
 my_globals['pd_smbg'] = 0
+my_globals['pd_settings'] = 0
+my_globals['bwz_settings'] = []
+
+table_columns_base_settings = ['Base Settings (editable)'] + list('%02d:00'%(a) for a in range(0,24))
+table_columns_derived_settings = copy.copy(table_columns_base_settings)
+table_columns_derived_settings[0] = 'Derived Settings'
+table_default_base_settings = []
+defaults_base = (('Insulin sens. (BG/u)',50),
+                 ('Food sens. (BG/g)',3.33),
+                 ('Insulin decay (hr)',4),
+                 ('Food decay (hr)',2),
+                 ('Liver glucose (BG/hr)',50))
+
+for row in defaults_base :
+    table_default_base_settings.append(dict())
+    table_default_base_settings[-1][-1] = row[0]
+    for i in range(24) :
+        table_default_base_settings[-1][i] = row[1]
 
 # for deployment, pass app.server (which is the actual flask app) to WSGI etc
 app = dash.Dash(external_stylesheets=external_stylesheets)
@@ -67,6 +92,45 @@ app.layout = html.Div(
                 ]
                  ),
 
+        dash_table.DataTable(id='base_settings_table',
+                             columns=[{"name": name, "id": str(i-1), "editable": (i!= 0)} for i,name in enumerate(table_columns_base_settings)],
+                             data=table_default_base_settings,
+                             style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                           'fontWeight': 'bold'
+                                           },
+                             style_cell={'height': 'auto',
+                                         # all three widths are needed
+                                         'minWidth': '3%','width': '3%', 'maxWidth': '3%',
+                                         'whiteSpace': 'normal',
+                                         },
+                             style_cell_conditional=[{'if': {'column_id': '-1'},'width': '15%'},
+                                                     {'if': {'column_id': '-1'},'textAlign': 'left'}],
+                             ),
+
+        dash_table.DataTable(id='derived_settings_table',
+                             editable=False,
+                             columns=[{"name": name, "id": str(i-1)} for i,name in enumerate(table_columns_derived_settings)],
+                             data=table_default_base_settings,
+                             style_header={'backgroundColor': 'rgb(230, 230, 230)',
+                                           'fontWeight': 'bold',
+                                           'color':'black',
+                                           },
+                             style_cell={'height': 'auto',
+                                         # all three widths are needed
+                                         'minWidth': '3%','width': '3%', 'maxWidth': '3%',
+                                         'whiteSpace': 'normal',
+                                         'color':'gray',
+                                         },
+                             style_cell_conditional=[{'if': {'column_id': '-1'},'width': '15%'},
+                                                     {'if': {'column_id': '-1'},'textAlign': 'left'},
+                                                     {'if': {'column_id': '-1'},'color': 'black'}],
+                             ),
+        html.Div(children=[
+                html.P('''* Units: "BG" stands for md/dL.'''),
+                html.P('''** To split a setting int two half-hour increments, type e.g. '50/60'.'''),
+                html.P('''*** "True basal rate" represents what your basal insulin should be, based on your sensitivity and liver glucose settings.'''),
+                ]),
+
         html.Label('User-input values:'),
         dcc.Input(id='my-id', value='initial value', type='text'),
         html.Div(id='my-div'),
@@ -82,12 +146,12 @@ app.layout = html.Div(
 #
 # Text callback
 #
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+# @app.callback(
+#     Output(component_id='my-div', component_property='children'),
+#     [Input(component_id='my-id', component_property='value')]
+# )
+# def update_output_div(input_value):
+#     return 'You\'ve entered "{}"'.format(input_value)
 
 #
 # How we parse the input file contents
@@ -126,7 +190,7 @@ def update_file(list_of_contents, list_of_names, list_of_dates):
 
     if list_of_contents is None:
         # Use the default file
-        my_globals['global_df'] = pd.read_json('Tidepool_Export_191104_191110.json')
+        my_globals['global_df'] = pd.read_json('download.json')
         list_of_names = ''
         list_of_dates = ''
         text_outputs = ['Using default file.']
@@ -153,7 +217,14 @@ def update_file(list_of_contents, list_of_names, list_of_dates):
             return text_outputs[find_errors.index(True)]
 
     print('updating global_smbg',file=sys.stdout)
-    my_globals['pd_smbg'] = my_globals['global_df'][my_globals['global_df']['type'] == 'smbg'][['deviceTime', 'value']]
+
+    data = my_globals['global_df']
+    my_globals['pd_smbg'] = data[data['type'] == 'smbg'][['deviceTime', 'value']]
+
+    #
+    # Settings
+    #
+    ManageSettings.LoadFromJsonData(data,my_globals)
 
     return '. '.join(text_outputs)
 
@@ -205,6 +276,43 @@ def update_dates_month(update_indicator):
 def update_dates_day(update_indicator):
 
     return max(np.array(my_globals['pd_smbg']['deviceTime'],dtype='datetime64'))
+
+#
+# Update derived table
+#
+@app.callback(Output('derived_settings_table', 'data'),
+              [Input('base_settings_table', 'data')])
+def update_derived_table(table):
+
+    print(table,file=sys.stdout)
+
+    i_isens = 0
+    i_fsens = 1
+    i_liver = 4
+
+    ret = []
+    ric = dict()
+    ric['-1'] = 'Carb-insulin ratio (g/u)'
+
+    basal = dict()
+    basal['-1'] = 'True basal rate (u/hr)'
+
+    for i in range(24) :
+        try :
+            ric[str(i)] = round(float(table[i_isens][str(i)])/float(table[i_fsens][str(i)]),1)
+        except (ValueError,TypeError) as e :
+            ric[str(i)] = 'ERR'
+
+        try :
+            basal[str(i)] = round(float(table[i_isens][str(i)])/float(table[i_liver][str(i)]),1)
+        except (ValueError,TypeError) as e :
+            basal[str(i)] = 'ERR'
+
+    ret.append(ric)
+    ret.append(basal)
+
+    return ret
+
 
 app.title = 'Kurt Webpage'
 
