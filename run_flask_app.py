5fb7059df9eee791c49ca02ad290f6ef5a5db399
brendlin <kurt.brendlinger@cern.ch>
2020-12-30 14:06:57 +0100

    Move flask_app (where layout is defined) to LayoutT1D

--- run_flask_app.py
+++ run_flask_app.py
@@ -4,7 +4,7 @@ from app import app
 import sys
 sys.path.insert(0, "../")
 
-from dashWebApplication.flask_app import layout
+from dashWebApplication.LayoutT1D import layout
 
 # This is apparently okay to deploy too.
 if __name__ == '__main__':
